382255fc8324465770e1941f6d3572b3c3df25d2
bySabi Files <flxinformatico@gmail.com>
2014-03-17 11:24:38 +0100

    more cleanup

---
+++ set-all-bridged-containers-v2_upstart/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+install -o root -m 644 "set-all-bridged-containers.conf" /etc/init/
--- youtrack/usr/jetty.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-exec \
-	setuser java \
-	$JETTY_HOME/bin/jetty.sh run
